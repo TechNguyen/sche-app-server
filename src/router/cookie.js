88a5ba7716a8a030a988f70da2fc1f6a7948bfd8
TechNguyen <ndt13102003@gmail.com>
2023-06-14 23:21:38 +0700

    fix

--- src/router/cookie.js
+++
@@ -1,6 +0,0 @@
-const express = require('express')
-const route = express.Router()
-const cookieController = require('../app/controllers/cookiesController')
-route.get('/cookie', cookieController.setcookie)
-route.get('/getcookie', cookieController.getUser)
-module.exports = route
